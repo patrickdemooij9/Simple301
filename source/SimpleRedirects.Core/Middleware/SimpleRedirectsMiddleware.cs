3a4c41b86cff8384eb55a79048dd2088888a6621
Patrick de Mooij <patrickdemooij98@hotmail.com>
2022-03-04 20:46:25 +0100

    Allow for media redirects

---
+++ source/SimpleRedirects.Core/Middleware/SimpleRedirectsMiddleware.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SimpleRedirects.Core.Options;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+
+namespace SimpleRedirects.Core.Middleware
+{
+    public class SimpleRedirectsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly RedirectRepository _redirectRepository;
+        private readonly IRuntimeState _runtimeState;
+        private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+
+        private SimpleRedirectsOptions _config;
+
+        public SimpleRedirectsMiddleware(RequestDelegate next,
+            RedirectRepository redirectRepository,
+            IRuntimeState runtimeState,
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IOptionsMonitor<SimpleRedirectsOptions> options)
+        {
+            _next = next;
+            _redirectRepository = redirectRepository;
+            _runtimeState = runtimeState;
+            _umbracoContextAccessor = umbracoContextAccessor;
+            _config = options.CurrentValue;
+            options.OnChange((newValue) => _config = newValue);
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (_runtimeState.Level != RuntimeLevel.Run)
+            {
+                await _next.Invoke(context);
+                return;
+            }
+
+            var pathAndQuery = context.Request.GetEncodedPathAndQuery();
+
+            if (pathAndQuery.IndexOf("/umbraco", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                await _next(context);
+                return;
+            }
+
+            if (_config.OnlyRedirectOn404)
+            {
+                context.Response.OnStarting(() =>
+                {
+                    if (context.Response.StatusCode != (int)HttpStatusCode.NotFound)
+                    {
+                        return Task.CompletedTask;
+                    }
+
+                    HandleRedirect(context);
+                    return Task.CompletedTask;
+                });
+                await _next(context);
+            }
+            else
+            {
+                if (!HandleRedirect(context))
+                {
+                    await _next(context);
+                }
+            }
+        }
+
+        private bool HandleRedirect(HttpContext context)
+        {
+            var url = new Uri(context.Request.GetEncodedUrl());
+            var matchedRedirect = _redirectRepository.FindRedirect(url);
+            if (matchedRedirect == null)
+            {
+                return false;
+            };
+
+            var isPerm = matchedRedirect.RedirectCode == (int)HttpStatusCode.MovedPermanently;
+            context.Response.Redirect(matchedRedirect.GetNewUrl(url), isPerm);
+            return true;
+        }
+    }
+}
--- source/SimpleRedirects.Core/RedirectUserComposer.cs
+++ source/SimpleRedirects.Core/RedirectUserComposer.cs
@@ -1,28 +1,48 @@
 using System.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleRedirects.Core.Components;
+using SimpleRedirects.Core.Middleware;
 using SimpleRedirects.Core.Options;
 using SimpleRedirects.Core.Utilities.Caching;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Web.Common.ApplicationBuilder;
 using Umbraco.Extensions;
 
 namespace SimpleRedirects.Core
 {
-    public class RedirectUserComposer : IUserComposer
+    public class RedirectUserComposer : IComposer
     {
         public void Compose(IUmbracoBuilder builder)
         {
             builder.Dashboards().Add<RedirectDashboard>();
             builder.Components().Append<DatabaseUpgradeComponent>();
-            builder.ContentFinders().InsertBefore<ContentFinderByUrl, RedirectContentFinder>();
 
             builder.Services.AddUnique<RedirectRepository>();
             builder.Services.AddUnique<ICacheManager, CacheManager>();
 
             builder.Services.Configure<SimpleRedirectsOptions>(builder.Config.GetSection(
-                                        SimpleRedirectsOptions.Position));
+                SimpleRedirectsOptions.Position));
+            var onlyRedirectOn404 = builder.Config.GetSection(SimpleRedirectsOptions.Position)?.Get<SimpleRedirectsOptions>()?.OnlyRedirectOn404 ?? false;
+
+            builder.Services.Configure<UmbracoPipelineOptions>(options => {
+                options.AddFilter(new UmbracoPipelineFilter(
+                    "SimpleRedirects",
+                    applicationBuilder =>
+                    {
+                        if (!onlyRedirectOn404)
+                            applicationBuilder.UseMiddleware<SimpleRedirectsMiddleware>();
+                    },
+                    applicationBuilder => {
+                        if (onlyRedirectOn404)
+                            applicationBuilder.UseMiddleware<SimpleRedirectsMiddleware>();
+                    },
+                    applicationBuilder => { }
+                ));
+            });
         }
     }
 }
