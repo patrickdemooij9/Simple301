9f664d42defdd5b006b93b7732fc947afcb9052d
Patrick de Mooij <patrickdemooij98@hotmail.com>
2022-06-28 20:48:46 +0200

    Cache refresher and Umbraco 10

---
+++ source/SimpleRedirects.Core/Notifications/RedirectCacheRefresherNotification.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Sync;
+
+namespace SimpleRedirects.Core.Notifications
+{
+    public class RedirectCacheRefresherNotification : CacheRefresherNotification
+    {
+        public RedirectCacheRefresherNotification(object messageObject, MessageType messageType) : base(messageObject, messageType)
+        {
+        }
+    }
+}
---
+++ source/SimpleRedirects.Core/Utilities/Caching/RedirectCacheRefresher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleRedirects.Core.Notifications;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Events;
+
+namespace SimpleRedirects.Core.Utilities.Caching
+{
+    public class RedirectCacheRefresher : CacheRefresherBase<RedirectCacheRefresherNotification>
+    {
+        public static readonly Guid UniqueId = Guid.Parse("fe3847bc-80c4-4ce0-abde-551bb409599a");
+
+        public RedirectCacheRefresher(AppCaches appCaches, IEventAggregator eventAggregator, ICacheRefresherNotificationFactory factory) : base(appCaches, eventAggregator, factory)
+        {
+        }
+
+        public override Guid RefresherUniqueId => UniqueId;
+        public override string Name => "Redirects Cache Refresher";
+
+        public override void RefreshAll()
+        {
+            AppCaches.RuntimeCache.ClearByKey(RedirectRepository.CacheCategoryKey);
+            base.RefreshAll();
+        }
+    }
+}
